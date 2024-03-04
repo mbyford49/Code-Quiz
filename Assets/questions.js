4fd4add5f2e30d70f445e61d7a2748528b753dea
mbyford49 <mbyford49@gmail.com>
2024-03-04 14:09:33 -0800

    questions added

---
+++ Assets/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "What year were the Seattle Mariners established:",
+        choices: ["1970", "1968", "1977", "1980"],
+        answer: "1977",
+    },
+
+    {
+        title: "Who was the first player to represent the Mariners in the Baseball Hall of Fame:",
+        choices: ["Randy Johnson", "Gaylord Perry", "Edgar Martinez", "Ken Griffey Jr."],
+        answer: "Ken Griffey Jr.",
+    },
+
+    {
+        title: "Who is the only pitcher to throw a perfect game in a Mariners uniform:",
+        choices: ["Jamie Moyer", "Randy Johnson", "Felix Hernandez", "Mark Langston"],
+        answer: "Felix Hernandez",
+    },
+
+    {
+        title: "What Mariner became just the second player in MLB history to win both MVP and Rookie of the Year honors in the same season:",
+        choices: ["Ken Griffey Jr.", "Alex Rodriguez", "Julio Rodriguez", "Ichiro Suzuki"],
+        answer: "Ichiro Suzuki",
+    },
+
+    {
+        title: "Who hit The Double, sending the Mariners to the 1995 ALCS:",
+        choices: ["Edgar Martinez", "Joey Cora", "Ken Griffey Jr.", "Jay Buhner"],
+        answer: "Edgar Martinez"
+    },
+];

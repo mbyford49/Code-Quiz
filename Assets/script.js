b3262fb1230a2ecad96822daf60da57916dfe6f2
mbyford49 <mbyford49@gmail.com>
2024-03-05 00:25:12 -0800

    cycles through questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,11 @@
-let time = 60
+let time = 60;
+var currentQuestionIndex = 0;
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var reactionEl = document.getElementById("reaction");
 var timer = document.querySelector("#start");
 
-function startTimer(){
+function playBall(){
     const htmlTochange=document.getElementById("time")
     htmlTochange.textContent=time
     let intervalId=setInterval(function(){
@@ -10,8 +14,41 @@ function startTimer(){
         if(time<1){
             clearInterval(intervalId)
         }
-    },1000)
+    },1000);
+    var introEl = document.getElementById("intro");
+    introEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestion();
 }
 
-timer.addEventListener("click", startTimer);
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl =document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+    for ( var i = 0; i < currentQuestion.choices.length; i++) {
+        var choice = currentQuestion.choices[i];
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+        choiceNode.textContent = i + 1 + ". " + choice;
+        choicesEl.appendChild(choiceNode);
+    }
+}
+
+function questionClick(event) {
+    var buttonEl = event.target;
+    console.log(buttonEl);
+    if(buttonEl.value === questions[currentQuestionIndex].answer) {
+        reactionEl.textContent = "Correct!";
+    } else {
+        reactionEl.textContent = "Nope!"
+    }
+    currentQuestionIndex++;
+    getQuestion();
+    
+}
+
+timer.addEventListener("click", playBall);
+choicesEl.onclick = questionClick;
 
